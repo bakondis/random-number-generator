cc35ebf2c52adba5b26d5a536223429f3de73a1b
bakondis <bakondis@gmail.com>
2023-07-12 13:12:14 +0200

    fixing package error

--- src/main/java/hu/bakondis/randomnumbergenerator/Swagger2SpringBoot.java
+++ src/main/java/hu/bakondis/randomnumbergenerator/Swagger2SpringBoot.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 @EnableOpenApi
-@ComponentScan(basePackages = { "io.swagger", "io.swagger.api" , "io.swagger.configuration"})
+@ComponentScan(basePackages = { "hu.bakondis.randomnumbergenerator", "hu.bakondis.randomnumbergenerator.api" , "hu.bakondis.randomnumbergenerator.configuration"})
 public class Swagger2SpringBoot implements CommandLineRunner {
 
     @Override
